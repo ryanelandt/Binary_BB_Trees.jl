a5c21780eefe4b920d68607dcff9d9fe8d317334
Ryan Elandt <ryan.elandt@gmail.com>
2018-09-19 21:19:39 -0400

    added test_exports.jl test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,7 @@ using StaticArrays
 using Binary_BB_Trees
 using Tri_Tet_Intersections  # for area, centroid
 
+
+include("test_exports.jl")
 include("intersection_tests.jl")
 include("mesh_box_tests.jl")
