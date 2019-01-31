6e7ea508af2271c0385ee1542632d9e822b476fb
“Ryan <ryan.elandt@gmail.com>
2019-01-31 11:32:00 -0500

    updated swept mesh funcitons

--- src/mesh_create_swept.jl
+++ src/mesh_create_swept.jl
@@ -19,25 +19,99 @@ function output_side_points(v1::Float64, the_fun::Function, width::Float64)
     return p1, p2, p5, p6
 end
 
-function create_swept_segment(width::Float64, v0::Float64, v1::Float64, the_fun::Function)
-    p1, p2, p5, p6 = output_side_points(v0, the_fun, width)
-    p3, p4, p7, p8 = output_side_points(v1, the_fun, width)
-    p9 = (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8) ./ 8
-    eM_box = output_eMesh_box(1.0)
-    empty!(eM_box.point)
-    push!(eM_box.point, p1, p2, p3, p4, p5, p6, p7, p8, p9)
+function create_swept_segment(width::Float64, v0::Float64, v1::Float64, the_fun::Function, open_neg_z::Bool, open_pos_z::Bool)
+    p1, p2, p3, p4 = output_side_points(v0, the_fun, width)
+    p5, p6, p7, p8 = output_side_points(v1, the_fun, width)
+    p_neg_z = (p1 + p2 + p3 + p4) ./ 4
+    p_pos_z = (p5 + p6 + p7 + p8) ./ 4
+    p_center = (p_neg_z + p_pos_z) ./ 4
+    eM_box = eMesh{Tri,Tet}()
+    append!(eM_box.ϵ, zeros(8))
+    push!(eM_box.ϵ, -1.0)
+    push!(eM_box.ϵ, -1.0)
+    push!(eM_box.ϵ, -1.0)
+    push!(eM_box.point, p1, p2, p3, p4, p5, p6, p7, p8, p_center, p_neg_z, p_pos_z)
+
+    # add non-z triangles
+    push!(eM_box.tri, SVector{3,Int64}(1,5,7))  # neg x
+    push!(eM_box.tri, SVector{3,Int64}(1,7,3))
+    push!(eM_box.tri, SVector{3,Int64}(2,4,8))  # pos x
+    push!(eM_box.tri, SVector{3,Int64}(2,8,6))
+    push!(eM_box.tri, SVector{3,Int64}(1,2,6))  # neg y
+    push!(eM_box.tri, SVector{3,Int64}(1,6,5))
+    push!(eM_box.tri, SVector{3,Int64}(3,7,8))  # pos y
+    push!(eM_box.tri, SVector{3,Int64}(3,8,4))
+
+    if open_neg_z
+        push!(eM_box.tri, SVector{3,Int64}(1,3,4))  # neg z
+        push!(eM_box.tri, SVector{3,Int64}(1,4,2))  # neg z
+    else
+        push!(eM_box.tri, SVector{3,Int64}(1,3,10))
+        push!(eM_box.tri, SVector{3,Int64}(3,4,10))
+        push!(eM_box.tri, SVector{3,Int64}(4,2,10))
+        push!(eM_box.tri, SVector{3,Int64}(2,1,10))
+    end
+
+    if open_pos_z
+        push!(eM_box.tri, SVector{3,Int64}(5,6,8))  # pos z
+        push!(eM_box.tri, SVector{3,Int64}(5,8,7))  # pos z
+    else
+        push!(eM_box.tri, SVector{3,Int64}(5,6,11))
+        push!(eM_box.tri, SVector{3,Int64}(6,8,11))
+        push!(eM_box.tri, SVector{3,Int64}(8,7,11))
+        push!(eM_box.tri, SVector{3,Int64}(7,5,11))
+    end
+
+    for k = 1:n_tri(eM_box)
+        push!(eM_box.tet, SVector{4,Int64}(9, eM_box.tri[k]...))
+    end
+
     return eM_box
 end
 
-function make_swept_mesh(width::Float64, the_fun::Function, lr)
+function make_swept_mesh_closed(width::Float64, the_fun::Function, lr)
     eM_box = eMesh{Tri,Tet}()
-    for k = 1:(length(lr) - 1)
-        eM_1 = create_swept_segment(width, lr[k], lr[k+1], the_fun)
+    n_segment = length(lr)
+    for k = 1:n_segment
+        eM_1 = create_swept_segment(width, lr[k], lr[mod1(k+1, n_segment)], the_fun, false, false)
         append!(eM_box, eM_1)
     end
     mesh_repair!(eM_box)
     return eM_box
 end
+
+function make_swept_mesh_open(width::Float64, the_fun::Function, lr)
+    eM_box = eMesh{Tri,Tet}()
+    n_segment = length(lr) - 1
+    for k = 1:n_segment
+        open_neg_z = (k == 1)
+        open_pos_z = (k == n_segment)
+        eM_1 = create_swept_segment(width, lr[k], lr[k+1], the_fun, open_neg_z, open_pos_z)
+        append!(eM_box, eM_1)
+    end
+    mesh_repair!(eM_box)
+    return eM_box
+end
+
+# function create_swept_segment(width::Float64, v0::Float64, v1::Float64, the_fun::Function)
+#     p1, p2, p5, p6 = output_side_points(v0, the_fun, width)
+#     p3, p4, p7, p8 = output_side_points(v1, the_fun, width)
+#     p9 = (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8) ./ 8
+#     eM_box = output_eMesh_box(1.0)
+#     empty!(eM_box.point)
+#     push!(eM_box.point, p1, p2, p3, p4, p5, p6, p7, p8, p9)
+#     return eM_box
+# end
+#
+# function make_swept_mesh(width::Float64, the_fun::Function, lr)
+#     eM_box = eMesh{Tri,Tet}()
+#     for k = 1:(length(lr) - 1)
+#         eM_1 = create_swept_segment(width, lr[k], lr[k+1], the_fun)
+#         append!(eM_box, eM_1)
+#     end
+#     mesh_repair!(eM_box)
+#     return eM_box
+# end
 
 
 
